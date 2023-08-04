e58b11ce2b3b1909af26777ec255d6d34e8785df
Samiron Saikia <sam18saikia@gmail.com>
2023-08-04 19:36:53 +0530

    Quiz Application

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,21 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "In 2023, Portugal Football team captain Cristiano Ronaldo joined the club Al Nassr of which country?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple","Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Germany","Saudi Arabia","UAE","Argentina"}
+    };
+
+    public static String correctAnswer[] = {
+            "Google","Notepad","Saudi Arabia"
+    };
+}
